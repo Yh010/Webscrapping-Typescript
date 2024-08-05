0a560cbf52c05ddd3dde6df00f1aafd8cb8ce5e1
Yash Hegde <yash4success@gmail.com>
2024-08-05 16:38:23 +0530

    adding to csv

--- dist/index.js
+++ dist/index.js
@@ -13,10 +13,37 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const axios_1 = __importDefault(require("axios"));
+const cheerio_1 = require("cheerio");
+const format_1 = require("@fast-csv/format");
+const products = [];
 function getResponseDatafromSite() {
     return __awaiter(this, void 0, void 0, function* () {
         const response = yield axios_1.default.get("https://www.scrapingcourse.com/ecommerce/");
-        console.log(response.data);
+        const data = response.data;
+        const $ = (0, cheerio_1.load)(data);
+        $("li.product").each((i, productHTMLElement) => {
+            const url = $(productHTMLElement).find("a").first().attr("href");
+            const image = $(productHTMLElement).find("img").first().attr("src");
+            const name = $(productHTMLElement).find("h2").first().text();
+            const price = $(productHTMLElement).find("span").first().text();
+            const product = {
+                Url: url,
+                Image: image,
+                Name: name,
+                Price: price
+            };
+            products.push(product);
+        });
+        // extract the data of interest from the product node
+        for (const product of products) {
+            console.log(product.Url);
+            console.log(product.Image);
+            console.log(product.Name);
+            console.log(product.Price);
+            console.log(`\n`);
+            console.log(`\n`);
+        }
+        (0, format_1.writeToPath)("products.csv", products, { headers: true }).on("error", error => console.error(error));
     });
 }
 getResponseDatafromSite();
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,50 @@
 import axios from "axios";
 import { load } from "cheerio"
+import { writeToPath } from "@fast-csv/format"
 
+type Product = {
+    Url?: string
+    Image?: string
+    Name?: string
+    Price?: string
+}
+
+const products: Product[] = [];
 async function getResponseDatafromSite() {
     const response = await axios.get("https://www.scrapingcourse.com/ecommerce/");
     const data = response.data;
 
     const $ = load(data);
 
-    const productHTMLElement = $("li.product").first()
+    $("li.product").each((i, productHTMLElement) => {
+        const url = $(productHTMLElement).find("a").first().attr("href")
+        const image = $(productHTMLElement).find("img").first().attr("src")
+        const name = $(productHTMLElement).find("h2").first().text()
+        const price = $(productHTMLElement).find("span").first().text()
 
-    // extract the data of interest from the product node
-    const url = $(productHTMLElement).find("a").first().attr("href")
-    const image = $(productHTMLElement).find("img").first().attr("src")
-    const name = $(productHTMLElement).find("h2").first().text()
-    const price = $(productHTMLElement).find("span").first().text()
+        const product: Product = {
+            Url: url,
+            Image: image,
+            Name: name,
+            Price: price
+        }
+
+        products.push(product);
+
+    })
 
 
-    console.log(url)
-    console.log(image)
-    console.log(name)
-    console.log(price)
+    for (const product of products) {
+        console.log(product.Url)
+        console.log(product.Image)
+        console.log(product.Name)
+        console.log(product.Price)
+        console.log(`\n`)
+        console.log(`\n`)
+    }
+
+    writeToPath("products.csv", products, { headers: true }).on("error", error => console.error(error));
+
 }
 
 getResponseDatafromSite();
