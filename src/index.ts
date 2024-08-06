cc7af91cd485746a49d5be03f70124ec40cca978
Yh010 <yash4success@gmail.com>
2024-08-06 11:38:39 +0530

    test

--- src/index.ts
+++ src/index.ts
@@ -76,5 +76,5 @@ async function scrapeSite() {
     writeToPath("products.csv", products, { headers: true })
         .on("error", error => console.error(error));
 }
-/*TODO: SCRAPING WITH HEADLESS BROWSER USING PUPPETER: https://www.zenrows.com/blog/puppeteer-web-scraping#use */
+/*TODO: SCRAPING WITH HEADLESS BROWSER USING PUPPETER: https://www.zenrows.com/blog/puppeteer-web-scraping#use wewww*/
 scrapeSite()
